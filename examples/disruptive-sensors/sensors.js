b20b17aa240f776ebd3b09d2a8707343743a27bf
Thomas Pedersen (thpeders) <thpeders@cisco.com>
2019-10-24 10:18:29 +0200

    Updated sensor example to filter  on specific sensors ids

--- examples/disruptive-sensors/sensors.js
+++ examples/disruptive-sensors/sensors.js
@@ -13,13 +13,23 @@ app.use(
 app.use(bodyParser.json());
 
 app.post('/',  (req, res) => {
-    //Filter on req.body.event.eventId to find the right sensor
+    //Filter on req.body.event.targetName to find the right sensor
+    //Project ID == blrpjplbffle57fk5fc0
     if (req.body.event.eventType === `temperature`) {
-        console.log(`Temperature:`+req.body.event.data.temperature.value); 
+        //Sensor ID bful62ho5b7g0093cucg
+        if (req.body.event.targetName === 'projects/blrpjplbffle57fk5fc0/devices/bful62ho5b7g0093cucg') {
+            console.log(`Hackathon 1 Temperature:`+req.body.event.data.temperature.value);     
+        }
     } else if (req.body.event.eventType === `touch`) {
-        console.log(`Touch received!`); 
+        //Sensor ID bfuld90lq8r000elc1jg
+        if (req.body.event.targetName === 'projects/blrpjplbffle57fk5fc0/devices/bfuld90lq8r000elc1jg') {
+            console.log(`Hackathon 1 Touch received!`); 
+        }
     } else if (req.body.event.eventType === `objectPresent`) {
-        console.log(`Proximity State:`+req.body.event.data.objectPresent.state); 
+        //Sensor ID bful880lq8r000elbshg
+        if (req.body.event.targetName === 'projects/blrpjplbffle57fk5fc0/devices/bful880lq8r000elbshg') {
+            console.log(`Hackathon 1 Proximity State:`+req.body.event.data.objectPresent.state); 
+        }
     }
     res.sendStatus(200);
 });
