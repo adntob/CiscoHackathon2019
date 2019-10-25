876f3334023cd00b9702d7cbdfc4e41d6c8fb20a
Adne Tob <aadne.t@live.com>
2019-10-25 16:06:17 +0200

    New features and icons

--- examples/websockets/Online.jsx
+++ examples/websockets/Online.jsx
@@ -1,11 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from '@momentum-ui/react';
+import OFFbulb from './img/OFFbulb.jpg'
+import ONbulb from './img/ONbulb.jpg'
+import logo from './img/logo.png'
 
-
-const QUESTIONS = [];
+const QUESTIONS = [
+  {
+    "Question": "What is love?",
+    "Answer": "Don't hurt me, no more"
+  }
+];
 
 const GROUPS = [
-
+{
+  'isActive':false,
+  'id':""
+}, 
+{
+  'isActive':true,
+  'id':""
+},
+ { 
+  'isActive':false,
+  'id':""
+ }
 ]
 
 
@@ -47,30 +65,52 @@ export default function Online(props) {
   }
 
 
-  function turnOnOffBulb(){
-    
-  } 
-
-
   const clearLog = () => { setEvents([]); };
 
   return (
     <div>
-      <h2>Scool.io{!hostname ? '' : ` on ${hostname}`}</h2>
+      <img src = {logo} width = {250}/>
+      <br/>
       <Button onClick={clearLog}>Clear log</Button>
-      <Button color="red" onClick={onDisconnect}>Log off</Button>
+      
       <Button  onClick ={Twerk}> Start quiz </Button>
 
       <div>Count: {count}</div>
+      <div>
+        {GROUPS.map( (group, key)=>{
+          if (!group.isActive){
+            
+            return <div key = {key}>
+              <p>Group number: {key} 
+
+                <img src = {OFFbulb} width={20} onClick = {turnOff} />
+              </p>
+            </div>
+          }
+          else{
+            return <div key = {key}>
+              <p>Group number: {key}
+              <img src = {ONbulb} width={20}/>
+              </p>
+              
+              </div>
+          }
+          
+        } )}
+      </div>
       <div>Temp: {temp}</div>
+
       <pre className="event-list">
         {events.map((event) => {
           const { timestamp, payload } = event;
           return `${timestamp.toLocaleTimeString('nb-NO')}: ${JSON.stringify(payload)}`;
         }).join('\n')}
       </pre>
+      <Button color="red" onClick={onDisconnect}>Log off</Button>
     </div>
   );
 }
 
-
+const turnOff = (i)=>{
+  console.log('swag lyf')
+}
